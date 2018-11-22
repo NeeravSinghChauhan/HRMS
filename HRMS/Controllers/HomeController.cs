e580af210fc2a3e1a88bd09a2485be50f35646f3
Neerav <neerav.its@gmail.com>
2018-11-22 12:02:36 +0530

    Test

--- HRMS/Controllers/HomeController.cs
+++ HRMS/Controllers/HomeController.cs
@@ -17,7 +17,7 @@ namespace HRMS.Controllers
 
         public IActionResult About()
         {
-            ViewData["Message"] = "Your application description page.";
+            ViewData["Message"] = "Your application description page.Neerav";
 
             return View();
         }
